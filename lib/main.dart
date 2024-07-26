63e4f00da917870d466e3c4618e02ebe3cb4a576
astha <astha111btit21@igdtuw.ac.in>
2024-07-27 00:55:37 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,12 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 230, 59, 130),
+            toolbarTextStyle: TextStyle(
+              color: Color.fromARGB(255, 230, 210, 217),
+            ),
+          ),
           brightness: Brightness.dark,
           primaryColor: const Color.fromARGB(255, 230, 59, 130),
           scaffoldBackgroundColor: const Color.fromARGB(255, 168, 3, 72),
