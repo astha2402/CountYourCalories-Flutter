54caa68fbc8c30e02971cb0158604c75212d8074
astha <astha111btit21@igdtuw.ac.in>
2024-07-31 19:58:18 +0530

    third

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const reusableContainerColor = Color.fromARGB(255, 233, 175, 214);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -11,32 +14,78 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: const Text('BMI CALCULATOR'),
       ),
       body: Column(children: <Widget>[
         Expanded(
             child: Row(children: <Widget>[
           Expanded(
-            child:
-                ReusableContainer(colour: Color.fromARGB(255, 233, 175, 214)),
+            child: ReusableContainer(
+                colour: reusableContainerColor,
+                cardChild: Column(
+                  children: const <Widget>[
+                    Icon(FontAwesomeIcons.mars, size: 80.0),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text('MALE')
+                  ],
+                )),
           ),
           Expanded(
-            child:
-                ReusableContainer(colour: Color.fromARGB(255, 233, 175, 214)),
+            child: ReusableContainer(
+                colour: reusableContainerColor,
+                cardChild: Column(
+                  children: const <Widget>[
+                    Icon(FontAwesomeIcons.mars, size: 80.0),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text('FEMALE')
+                  ],
+                )),
           ),
         ])),
         Expanded(
-          child: ReusableContainer(colour: Color.fromARGB(255, 233, 175, 214)),
+          child: ReusableContainer(
+              colour: reusableContainerColor,
+              cardChild: Column(
+                children: const <Widget>[
+                  Icon(FontAwesomeIcons.mars, size: 80.0),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text('MALE')
+                ],
+              )),
         ),
         Expanded(
             child: Row(children: <Widget>[
           Expanded(
-            child:
-                ReusableContainer(colour: Color.fromARGB(255, 233, 175, 214)),
+            child: ReusableContainer(
+                colour: reusableContainerColor,
+                cardChild: Column(
+                  children: const <Widget>[
+                    Icon(FontAwesomeIcons.mars, size: 80.0),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text('MALE')
+                  ],
+                )),
           ),
           Expanded(
-            child:
-                ReusableContainer(colour: Color.fromARGB(255, 233, 175, 214)),
+            child: ReusableContainer(
+                colour: reusableContainerColor,
+                cardChild: Column(
+                  children: const <Widget>[
+                    Icon(FontAwesomeIcons.mars, size: 80.0),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text('MALE')
+                  ],
+                )),
           ),
         ]))
       ]),
@@ -45,14 +94,16 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableContainer extends StatelessWidget {
-  ReusableContainer({required this.colour}) {}
+  ReusableContainer({required this.colour, required this.cardChild});
 
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(10.0),
+        child: cardChild,
+        margin: const EdgeInsets.all(10.0),
         decoration: BoxDecoration(
             color: colour, borderRadius: BorderRadius.circular(10.0)));
   }
